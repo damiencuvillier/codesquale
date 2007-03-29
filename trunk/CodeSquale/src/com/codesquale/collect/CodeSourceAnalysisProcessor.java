fefd3ed5ba12c2540fb118a7496ead28cb3f2ba2
damien.willier <damien.willier@7896da65-2a2b-0410-bec6-9377b3eb570b>
2007-03-29 10:06:39 +0000

    Creating the class wich processes code audit.

---
+++ trunk/CodeSquale/src/com/codesquale/collect/CodeSourceAnalysisProcessor.java
@@ -0,0 +1,12 @@
+package com.codesquale.collect;
+
+import com.codesquale.file.RepositoryBrowser;
+
+public class CodeSourceAnalysisProcessor {
+	
+	
+	public void Process(RepositoryBrowser projectToAnalyse)
+	{
+	}
+
+}
