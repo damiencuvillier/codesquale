2549bbf514c5fedf79205364081c768fcef5fa47
damien.cuvillier <damien.cuvillier@7896da65-2a2b-0410-bec6-9377b3eb570b>
2007-03-29 12:49:37 +0000

    Bug Corrects DirectoriesList FilesList Filtering

--- CodeSquale/src/com/codesquale/file/DirectoryElement.java
+++ CodeSquale/src/com/codesquale/file/DirectoryElement.java
@@ -6,20 +6,22 @@ import java.util.Vector;
 
 import com.codesquale.exceptions.NotDirectoryException;
 
-
 /**
  * Tree Element which represents a directoruy
+ * 
  * @author DCUVILLIER
- *
+ * 
  */
-public class DirectoryElement extends AbstractElement{
+public class DirectoryElement extends AbstractElement {
 	private static org.apache.log4j.Logger logger = org.apache.log4j.Logger
-		.getLogger(DirectoryElement.class);
-	
+			.getLogger(DirectoryElement.class);
+
 	/**
 	 * List of included files & directories
 	 */
-	private TreeSet<AbstractElement> listFiles = null;
+	private TreeSet<AbstractElement> globalList = null;
+	private Vector<DirectoryElement> directoriesList = null;
+	private Vector<FileElement> filesList = null;
 	
 	
 	/**
@@ -29,35 +31,62 @@ public class DirectoryElement extends AbstractElement{
 	 */
 	public DirectoryElement(java.io.File dir) throws NotDirectoryException {
 		super(dir);
-		if(! dir.isDirectory())
+		if (!dir.isDirectory())
 			throw new NotDirectoryException(dir);
-		listFiles = new TreeSet<AbstractElement>();
+		globalList = new TreeSet<AbstractElement>();
+		directoriesList = new Vector<DirectoryElement>();
+		filesList = new Vector<FileElement>();
 		browseFiles(dir);
 	}
-	
+
 	/**
 	 * Browses all files with the specified filter
+	 * 
 	 * @param dir
 	 */
-	private void browseFiles(java.io.File dir, Vector<Integer> filter ){
+	private void browseFiles(java.io.File dir, FileFilter filter) {
 		File[] list = dir.listFiles();
-		for (int i = 0; i<list.length; i++){
+		for (int i = 0; i < list.length; i++) {
 			File file = list[i];
-			if(file.isDirectory())
+			if (file.isDirectory())
 				try {
-					listFiles.add(new DirectoryElement(file));
+					DirectoryElement directoryElement = new DirectoryElement(file);
+					globalList.add(directoryElement);
+					directoriesList.add(directoryElement);
 				} catch (NotDirectoryException e) {
 					logger.error(e.getMessage());
 				}
-			if(file.isFile())
-				if(filter != null)
-				listFiles.add(new FileElement(file));
+			if (file.isFile()) {
+				FileElement fileElement = new FileElement(file);
+				if (filter == null
+						|| filter.isAllowed(fileElement.getExtension())){
+					globalList.add(fileElement);
+					filesList.add(fileElement);
+				}
+				
+			}
+
 		}
 	}
-	private void browseFiles(java.io.File dir){
+
+	private void browseFiles(java.io.File dir) {
 		browseFiles(dir, null);
 	}
 
+	public TreeSet<AbstractElement> getGlobalList() {
+		return globalList;
+	}
+
+	public Vector<DirectoryElement> getDirectoriesList() {
+		return directoriesList;
+	}
+
+	public Vector<FileElement> getFilesList() {
+		return filesList;
+	}
 	
-	
+	public String toString() {
+		String superMessage = "";
+		return superMessage;
+	}
 }
