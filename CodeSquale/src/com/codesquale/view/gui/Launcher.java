9ba39dea1b3f7f65071bcfffdf9c029a2cea3b0e
raphael.bittel <raphael.bittel@7896da65-2a2b-0410-bec6-9377b3eb570b>
2007-04-17 13:07:40 +0000

    Updating with log4j out to the GUI

--- CodeSquale/src/com/codesquale/view/gui/Launcher.java
+++ CodeSquale/src/com/codesquale/view/gui/Launcher.java
@@ -5,17 +5,15 @@ package com.codesquale.view.gui;
 public class Launcher {
 
 	/**
-	 * Test Class for project
-	 * 
 	 * Launch Repository Browser Which calls the parsing process
-	 * @author DCUVILLIER
+	 * @author rbittel
 	 *
 	 */
 
 		
 	public static void main(String[] args) 
 	{
-		Console csConsole = new Console();	
+		new Console();	
 	}
 
 }
