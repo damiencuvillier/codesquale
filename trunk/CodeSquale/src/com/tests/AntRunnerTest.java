7a739fbd3f2ee2e3ea7a65993da91cf6c946bba2
mathieu.bourguignon <mathieu.bourguignon@7896da65-2a2b-0410-bec6-9377b3eb570b>
2007-04-19 09:29:30 +0000

    Test Case for AntRunner Singleton

--- trunk/CodeSquale/src/com/tests/AntRunnerTest.java
+++ trunk/CodeSquale/src/com/tests/AntRunnerTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.util.HashMap;
 
 import junit.framework.JUnit4TestAdapter;
+import junit.framework.TestCase;
 
 import org.apache.log4j.Logger;
 import org.junit.Before;
@@ -12,48 +13,83 @@ import org.junit.Test;
 
 import com.codesquale.ant.AntRunner;
 
-public class AntRunnerTest {
+/**
+ * 
+ * @author mbourguignon
+ * Test Case for AntRunner Singleton
+ */
+public class AntRunnerTest extends TestCase{
 	
-	private AntRunner myAntRunner;
+	private AntRunner runner=null; 
 	private HashMap myHash = null;
 	
 	private static Logger logger = Logger.getLogger(AntRunnerTest.class);
 	
-	public AntRunnerTest()
+	public AntRunnerTest(String name)
 	{
-		
+		super(name);
 	}
-	
 	public static junit.framework.Test suite() {
-		 return new JUnit4TestAdapter(AntRunner.class);
+		 return new JUnit4TestAdapter(ParsingUnitTest.class);
 	}
-	
 	@Before public void setUp()
 	{
 		myHash = new HashMap();
-		myAntRunner = AntRunner.getInstance();
+		runner = AntRunner.getInstance();
 	}
 	
-	@Test public void testAntResults()
+	@Test 
+	public void testAntResults()
 	{
 		try
 		{
-			myAntRunner.init("testsrc/AntScript.xml", "testsrc");
+			runner.init("AntScript.xml", "xml");
+			System.out.println("Init");
+			
 			myHash.put("event", "results");
 			myHash.put("subject", "Results Target");
 			myHash.put("message", "Testing Results ant target");
-			myAntRunner.setProperties(myHash, false);
 			
-			boolean result = myAntRunner.runTarget("results");
+			runner.setProperties(myHash, false);
+			System.out.println("Properties");
+			
+			
+			boolean result = runner.runTarget("results");
+			System.out.println("Run");
 			
 			assertEquals(result, true);
+
 			
 		}catch(Exception e){
-			logger.fatal("Error in testAntResults");
+			logger.fatal(e.getMessage());
 		}
 		
 		
 	}
+	
+//	public static void main(String[] args)
+//	{
+//		
+//		
+//		logger.info(Messages.getString("LIB_FILTER_INIT")); //$NON-NLS-1$
+//		FileFilter filter = new FileFilter();
+//		filter.addFileType(FileFilter.JAVA_SOURCEFILE);
+//		logger.info(Messages.getString("LIB_BROWSING_DIR")); //$NON-NLS-1$
+//		ProjectBrowser browser;
+//		try {
+//			browser = new ProjectBrowser(new File("Z:\\Livrables\\alpha.V0.1\\V0.1\\jedit"),new File("output.xml"),filter);
+//			
+//			browser.ProcessAnalysis();
+//			browser.ProcessDescription();
+//			
+//		} catch (NotDirectoryException e) {
+//			// TODO Bloc catch auto-généré
+//			e.printStackTrace();
+//		}
+//		
+//		
+//		testAntResults();
+//	}
 	
 	
 
