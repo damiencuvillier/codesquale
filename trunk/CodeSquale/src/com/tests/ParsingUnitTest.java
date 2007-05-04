78ef12583d01a3be98dafa5bf47c573f38a11f4d
mathieu.bourguignon <mathieu.bourguignon@7896da65-2a2b-0410-bec6-9377b3eb570b>
2007-05-04 12:25:21 +0000

    [UPD] Parsing one file Test

--- trunk/CodeSquale/src/com/tests/ParsingUnitTest.java
+++ trunk/CodeSquale/src/com/tests/ParsingUnitTest.java
@@ -3,15 +3,22 @@ package com.tests;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 import junit.framework.JUnit4TestAdapter;
 
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
+
+import antlr.ANTLRException;
 
 import com.codesquale.parser.ParsingUnit;
 
 public class ParsingUnitTest {
+	
+	private static Logger logger = Logger.getLogger(ParsingUnitTest.class);
 	
 	private ParsingUnit myParsingUnit = null;
 	private File myTestFile = null;
@@ -23,8 +30,32 @@ public class ParsingUnitTest {
 	
 	@Before public void setUp()
 	{
+	
 		myParsingUnit = new ParsingUnit();
-		myTestFile = new File("ClassMoney.javatest");
+		myTestFile = new File("test\\ClassMoney.javatest");	
+	}
+	
+	@Test
+	public void parseFile()
+	{
+		try
+		{		
+			myParsingUnit.DoParse(myTestFile);
+					
+			FileOutputStream xmlFile = myParsingUnit.ASTToXML("ClassMoney.xml");
+			
+			if(xmlFile.equals(null))
+			{
+				fail("error xml file can't be null");
+			 	throw new Exception();
+			}
+		}
+		catch(Exception e)
+		{
+			logger.fatal(e);
+			e.printStackTrace();
+			fail("error in file parsing");
+		}
 	}
 
 }
