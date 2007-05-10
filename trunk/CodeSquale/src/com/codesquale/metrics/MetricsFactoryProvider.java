ba5fe7a1d7df54541911971ea4226ee726c1e5d2
damien.willier <damien.willier@7896da65-2a2b-0410-bec6-9377b3eb570b>
2007-05-10 10:20:34 +0000

    [UPD]Renaming  AbstractMetricsFactory to MetricsFactoryprovider

--- trunk/CodeSquale/src/com/codesquale/metrics/MetricsFactoryProvider.java
+++ trunk/CodeSquale/src/com/codesquale/metrics/MetricsFactoryProvider.java
@@ -2,14 +2,30 @@ package com.codesquale.metrics;
 
 import com.codesquale.metrics.saxon.SaxonMetricsFactory;
 
-public class AbstractMetricsFactory 
+public class MetricsFactoryProvider 
 {
+	
+	private static MetricsFactoryProvider _instance = null;
+
+	private MetricsFactoryProvider()
+	{
+	}
+	
+	
+	public static MetricsFactoryProvider getInstance()
+	{
+		if(_instance == null) _instance = new MetricsFactoryProvider();
+		
+		return _instance;
+	}
+	
 	public static class MetricsFactoryType
 	{
 		public static MetricsFactoryType SAXON_FACTORY;
 		public static MetricsFactoryType EXIST_FACTORY;
 	}
 	
+
 	public IMetricsFactory GetMetricsFactory(MetricsFactoryType type) 
 	{
 		if(type == MetricsFactoryType.SAXON_FACTORY)
