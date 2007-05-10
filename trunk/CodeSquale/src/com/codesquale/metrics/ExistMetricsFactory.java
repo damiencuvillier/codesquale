cd7525340bccfd77d3449c7c213f8a194a714fdb
damien.willier <damien.willier@7896da65-2a2b-0410-bec6-9377b3eb570b>
2007-05-10 08:03:48 +0000

    [ADD] Xquery Implementation

---
+++ trunk/CodeSquale/src/com/codesquale/metrics/ExistMetricsFactory.java
@@ -0,0 +1,19 @@
+package com.codesquale.metrics;
+
+//  @ Project : Untitled
+//  @ File Name : ExistMetricsFactory.java
+//  @ Date : 10/05/2007
+//  @ Author : 
+//
+//
+
+
+
+
+public class ExistMetricsFactory extends IMetricsFactory 
+{
+	public void GenerateResultsFile() 
+	{
+	
+	}
+}
