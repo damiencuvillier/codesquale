7901c9953a67e6e6433f66458ea3f37ec4f58af8
damien.willier <damien.willier@7896da65-2a2b-0410-bec6-9377b3eb570b>
2007-05-10 10:13:29 +0000

    [UPD]Creating the AbstractMetricsFactory which provides the desired XQuery Implementation

--- trunk/CodeSquale/src/com/codesquale/metrics/AbstractMetricsFactory.java
+++ trunk/CodeSquale/src/com/codesquale/metrics/AbstractMetricsFactory.java
@@ -1,9 +1,20 @@
 package com.codesquale.metrics;
 
+import com.codesquale.metrics.saxon.SaxonMetricsFactory;
+
 public class AbstractMetricsFactory 
 {
-	public void GetMetricsFactory(String type) 
+	public static class MetricsFactoryType
 	{
+		public static MetricsFactoryType SAXON_FACTORY;
+		public static MetricsFactoryType EXIST_FACTORY;
+	}
 	
+	public IMetricsFactory GetMetricsFactory(MetricsFactoryType type) 
+	{
+		if(type == MetricsFactoryType.SAXON_FACTORY)
+			return new SaxonMetricsFactory();
+		
+		return null;	
 	}
 }
--- trunk/CodeSquale/src/com/codesquale/metrics/saxon/SaxonMetricsFactory.java
+++ trunk/CodeSquale/src/com/codesquale/metrics/saxon/SaxonMetricsFactory.java
@@ -1,23 +1,8 @@
 package com.codesquale.metrics.saxon;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
-
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-
 import org.apache.log4j.Logger;
-
-import net.sf.saxon.Configuration;
-import net.sf.saxon.query.DynamicQueryContext;
-import net.sf.saxon.query.StaticQueryContext;
-import net.sf.saxon.query.XQueryExpression;
-import net.sf.saxon.trans.XPathException;
-
-import com.codesquale.utils.*;
 import com.codesquale.metrics.*;
 
 
@@ -27,7 +12,7 @@ public class SaxonMetricsFactory implements IMetricsFactory {
 	private static Logger logger = Logger.getLogger(SaxonMetricsFactory.class);
 
 	@SuppressWarnings("deprecation")
-	public void generateResultFile(String fullPathSourceFile, String fullPathResultFile)
+	public void CalculateCountersFromSourceFile(String fullPathSourceFile, String fullPathResultFile)
 	{
 		logger.debug("Processing Xquery from the file... " + fullPathSourceFile);
 		
