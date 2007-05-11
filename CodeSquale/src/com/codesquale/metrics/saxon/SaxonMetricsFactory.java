fcde928332a421ac7fafe1857ea500f5a77a5f30
damien.willier <damien.willier@7896da65-2a2b-0410-bec6-9377b3eb570b>
2007-05-11 10:28:31 +0000

    [UPD] Decoupling the Saxon Xquery Implementation

--- CodeSquale/src/com/codesquale/metrics/saxon/SaxonMetricsFactory.java
+++ CodeSquale/src/com/codesquale/metrics/saxon/SaxonMetricsFactory.java
@@ -1,5 +1,15 @@
 package com.codesquale.metrics.saxon;
 
+import java.io.File;
+import java.util.Properties;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.stream.StreamResult;
+
+import net.sf.saxon.om.NodeInfo;
+import net.sf.saxon.om.SequenceIterator;
+import net.sf.saxon.trans.XPathException;
+
 import org.apache.log4j.Logger;
 import com.codesquale.metrics.*;
 
@@ -13,18 +23,53 @@ public class SaxonMetricsFactory implements IMetricsFactory {
 	public void CalculateCountersFromSourceFile(String fullPathSourceFile, String fullPathResultFile)
 	{
 		logger.debug("Processing Xquery counting from the file " + fullPathSourceFile+" ...");
-		
+		// Setting to SaxonProcessor the file to analyze
 		SaxonProcessor.getInstance().setXMLSourceDocument(fullPathSourceFile);
-		SaxonProcessor.getInstance().ExecuteSingleFileCountingQuery(fullPathResultFile);
+		ExecuteSingleFileCountingQuery(fullPathResultFile);
 		
 
 		// Getting info about file which has been processed
 		SaxonProcessor.getInstance().setXMLSourceDocument(fullPathResultFile);
-		
-		//System.out.println(SaxonProcessor.getInstance().GetNumberOfClasses() + "classes");
+		// Updating the project counters
+		ProjectGlobalCounters.getInstance().incrementNumberOfClasses(GetNumberOfClasses());
 		
 
 		logger.debug("Xquery counting process finished "+ fullPathResultFile + " generated ...");
+	}
+	
+	
+	public void ExecuteSingleFileCountingQuery(String outFileFullPath)
+	{
+		Properties props = new Properties();
+		props.setProperty(OutputKeys.METHOD, "xml");
+		props.setProperty(OutputKeys.INDENT, "yes");
+		
+		try {
+			SaxonQueryProvider.getInstance().getsingleFileCountingQueryObject().run
+				(SaxonProcessor.getInstance().getDynamicQueryContext(), new StreamResult(new File(outFileFullPath)), props);
+		} catch (XPathException e) {
+			logger.fatal(e.getMessage());
+		}
+	}
+	
+	private int GetNumberOfClasses()
+	{
+		try {
+			SequenceIterator classesIterator = 
+				SaxonQueryProvider.getInstance().getnumberOfClassesQueryObject().iterator
+				(SaxonProcessor.getInstance().getDynamicQueryContext());
+
+			while (true && classesIterator != null) 
+			{
+			    NodeInfo classesValue = (NodeInfo)classesIterator.next();
+			    if (classesValue==null) break;
+			    return Integer.parseInt(classesValue.getStringValue());
+			}   
+			
+		} catch (XPathException e) {
+			logger.fatal(e.getMessage());
+		}
+		return -1;
 	}
 
 }
