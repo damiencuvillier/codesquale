4ba682d3218c36d021bb45ac20d80d38eb476187
damien.willier <damien.willier@7896da65-2a2b-0410-bec6-9377b3eb570b>
2007-05-11 10:28:31 +0000

    [UPD] Decoupling the Saxon Xquery Implementation

--- trunk/CodeSquale/src/com/codesquale/metrics/saxon/SaxonProcessor.java
+++ trunk/CodeSquale/src/com/codesquale/metrics/saxon/SaxonProcessor.java
@@ -18,7 +18,7 @@ import net.sf.saxon.trans.XPathException;
 
 import org.apache.log4j.Logger;
 
-import com.codesquale.utils.Utilities;
+
 
 public class SaxonProcessor 
 {
@@ -30,55 +30,12 @@ public class SaxonProcessor
 	private StaticQueryContext staticContext = null;
 	private DynamicQueryContext dynamicContext = null;
 	
-	
-	private XQueryExpression singleFileCountingQuery = null;
-	private XQueryExpression numberOfClasses = null;
-	
+
 	public static SaxonProcessor getInstance()
 	{
 		if(_instance == null) _instance = new SaxonProcessor();
 		
 		return _instance;
-	}
-	
-	
-	public void ExecuteSingleFileCountingQuery(String outFileFullPath)
-	{
-		Properties props = new Properties();
-		props.setProperty(OutputKeys.METHOD, "xml");
-		props.setProperty(OutputKeys.INDENT, "yes");
-		
-		File outputFile = new File(outFileFullPath);
-		
-		StreamResult serializedFile =  new StreamResult(outputFile);
-		
-		
-		try {
-			System.out.println(outFileFullPath);
-			System.out.println(dynamicContext);
-			singleFileCountingQuery.run(dynamicContext,serializedFile,props);
-			System.out.println("Test2");
-		} catch (XPathException e) {
-			logger.fatal(e.getMessage());
-		}
-	}
-	
-	public int GetNumberOfClasses()
-	{
-		try {
-			SequenceIterator classesIterator = numberOfClasses.iterator(dynamicContext);
-
-			while (true && classesIterator != null) 
-			{
-			    NodeInfo classesValue = (NodeInfo)classesIterator.next();
-			    if (classesValue==null) break;
-			    return Integer.parseInt(classesValue.getStringValue());
-			}   
-			
-		} catch (XPathException e) {
-			logger.fatal(e.getMessage());
-		}
-		return -1;
 	}
 	
 	
@@ -100,10 +57,21 @@ public class SaxonProcessor
 		}
 	}
 	
+	public StaticQueryContext getStaticQueryContext()
+	{
+		return staticContext;
+	}
+	
+	
+	public DynamicQueryContext getDynamicQueryContext()
+	{
+		return dynamicContext;
+	}
+	
+	
 	private SaxonProcessor()
 	{
 		initProcessor();
-		compileQuery();
 	}
 	
 	private void initProcessor()
@@ -116,25 +84,6 @@ public class SaxonProcessor
 		dynamicContext = new DynamicQueryContext(config);
 	}
 	
-	private void compileQuery()
-	{
-		String singleFileCountingQueryString;
-		
-		try {
-			 singleFileCountingQueryString = Utilities.readFileAsString(SaxonQueryProvider.SINGLE_FILE_COUNTING);
-			 singleFileCountingQuery = staticContext.compileQuery(singleFileCountingQueryString);
-			  
-			 numberOfClasses = staticContext.compileQuery("//directoryResult/counters/classes/all");
-			 
-		} catch (IOException e) 
-		{
-			logger.fatal(e.getMessage());
-		}
-		catch (XPathException ex) 
-		{
-			logger.fatal(ex.getMessage());
-		} 
-	}
 	
 	
 }
--- trunk/CodeSquale/src/com/codesquale/metrics/saxon/SaxonQueryProvider.java
+++ trunk/CodeSquale/src/com/codesquale/metrics/saxon/SaxonQueryProvider.java
@@ -1,7 +1,114 @@
 package com.codesquale.metrics.saxon;
 
+import java.io.IOException;
+
+import net.sf.saxon.expr.StaticContext;
+import net.sf.saxon.query.StaticQueryContext;
+import net.sf.saxon.query.XQueryExpression;
+import net.sf.saxon.trans.XPathException;
+
+import org.apache.log4j.Logger;
+
+import com.codesquale.utils.Utilities;
+
 public class SaxonQueryProvider {
 	
-	public final static String SINGLE_FILE_COUNTING = "xml\\XQuery\\saxon\\DirectoryFileCounters.xquery";
+	private static Logger logger = Logger.getLogger(SaxonProcessor.class);
+	
+	private final String SINGLE_FILE_COUNTING_QUERY_PATH = "xml\\XQuery\\saxon\\DirectoryFileCounters.xquery";
 
+	private XQueryExpression singleFileCountingQuery = null;
+	private XQueryExpression numberOfClasses = null;
+	private XQueryExpression numberOfPrivateClasses = null;
+	private XQueryExpression numberOfPublicClasses = null;
+	
+	private static SaxonQueryProvider _instance = null;
+	
+	public static SaxonQueryProvider getInstance()
+	{
+		if(_instance==null) _instance = new SaxonQueryProvider();
+		
+		return _instance;
+	}
+	
+	private SaxonQueryProvider()
+	{
+		compileQuery();
+	}
+	
+	private void compileQuery()
+	{
+		StaticQueryContext context = SaxonProcessor.getInstance().getStaticQueryContext(); 
+		
+		try 
+		{
+			 singleFileCountingQuery = context.compileQuery(getSingleFileCountingQuery());
+			 numberOfClasses = context.compileQuery(getNumberOfClassesQuery());
+			 numberOfPrivateClasses = context.compileQuery(getNumberOfPublicClassesQuery()); 
+			 numberOfPublicClasses = context.compileQuery(getNumberOfPrivateClassesQuery()); 
+		} 
+		catch (XPathException ex) 
+		{
+			logger.fatal(ex.getMessage());
+		} 
+	}
+	
+	/*
+	 *  XQuery object
+	 */
+	
+	
+	public XQueryExpression getnumberOfClassesQueryObject()
+	{
+		return numberOfClasses ;
+	}
+	
+	public XQueryExpression getNumberOfPrivateClassesQueryObject()
+	{
+		return numberOfPrivateClasses ;
+	}
+	
+	public XQueryExpression getsnumberOfPublicClassesQueryObject()
+	{
+		return numberOfPublicClasses ;
+	}
+	
+	public XQueryExpression getsingleFileCountingQueryObject()
+	{
+		return singleFileCountingQuery ;
+	}
+	
+	
+	/*
+	 *  Query litterals
+	 */
+	
+	private String getSingleFileCountingQuery()
+	{
+		try 
+		{
+			return Utilities.readFileAsString(SINGLE_FILE_COUNTING_QUERY_PATH);
+		} catch (IOException e) 
+		{
+			logger.fatal("The XQuery file seems to be unavailable.");
+		}
+		
+		return null;
+	}
+	
+	private  String getNumberOfClassesQuery()
+	{
+		return "//directoryResult/counters/classes/all";
+	}
+	
+	private  String getNumberOfPrivateClassesQuery()
+	{
+		return "//directoryResult/counters/classes/private";
+	}
+	
+	private  String getNumberOfPublicClassesQuery()
+	{
+		return "//directoryResult/counters/classes/public";
+	}
+	
 }
