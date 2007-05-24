2acd0177daebfe7c6cf70b91f66e9868c8c3b336
damien.willier <damien.willier@7896da65-2a2b-0410-bec6-9377b3eb570b>
2007-05-24 12:56:42 +0000

    [UPD] Adding the JavaDoc to the XQuery Saxon implementation.

--- CodeSquale/src/com/codesquale/metrics/saxon/SaxonProcessor.java
+++ CodeSquale/src/com/codesquale/metrics/saxon/SaxonProcessor.java
@@ -1,89 +1,100 @@
 package com.codesquale.metrics.saxon;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.Properties;
 
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
 import net.sf.saxon.Configuration;
-import net.sf.saxon.om.NodeInfo;
-import net.sf.saxon.om.SequenceIterator;
 import net.sf.saxon.query.DynamicQueryContext;
 import net.sf.saxon.query.StaticQueryContext;
-import net.sf.saxon.query.XQueryExpression;
 import net.sf.saxon.trans.XPathException;
 
 import org.apache.log4j.Logger;
 
-
-
-public class SaxonProcessor 
-{
+/**
+ * 
+ * This class is the encapsulation of the saxon implementation. It provides the
+ * few essential functionnalities of Saxon such as "setting the source xml
+ * document to proceed xquery on", "providing the execution context", etc.
+ * 
+ * @author dwillier
+ * 
+ */
+public class SaxonProcessor {
 	private static Logger logger = Logger.getLogger(SaxonProcessor.class);
-	
-	private static SaxonProcessor _instance= null;
-	
-	private Configuration config = null;
-	private StaticQueryContext staticContext = null;
-	private DynamicQueryContext dynamicContext = null;
-	
 
-	public static SaxonProcessor getInstance()
-	{
-		if(_instance == null) _instance = new SaxonProcessor();
-		
+	private static SaxonProcessor _instance = null;
+
+	private Configuration config = null;
+
+	private StaticQueryContext staticContext = null;
+
+	private DynamicQueryContext dynamicContext = null;
+
+	public static SaxonProcessor getInstance() {
+		if (_instance == null)
+			_instance = new SaxonProcessor();
+
 		return _instance;
 	}
-	
-	
+
+	/**
+	 * Sets the XML file which the xquery have to be processed on.
+	 * 
+	 * @param inputFilePath
+	 *            Full path of the XML file.
+	 */
 	@SuppressWarnings("deprecation")
-	public void setXMLSourceDocument(String inputFilePath)
-	{
+	public void setXMLSourceDocument(String inputFilePath) {
 		// Preparing the single file to analyze
 		File inputFile = null;
 		StreamSource inputStreamSource = null;
-		
+
 		// Loading the file to analyze
 		inputFile = new File(inputFilePath);
 		inputStreamSource = new StreamSource(inputFile);
-		
+
 		try {
-			dynamicContext.setContextNode(config.buildDocument(inputStreamSource));
+			dynamicContext.setContextNode(config
+					.buildDocument(inputStreamSource));
 		} catch (XPathException e) {
 			logger.fatal(e.getMessage());
 		}
 	}
-	
-	public StaticQueryContext getStaticQueryContext()
-	{
+
+	/**
+	 * Provides the static query context of Saxon.
+	 * 
+	 * @return The StaticQueryContext object initialized.
+	 */
+	public StaticQueryContext getStaticQueryContext() {
 		return staticContext;
 	}
-	
-	
-	public DynamicQueryContext getDynamicQueryContext()
-	{
+
+	/**
+	 * Provides the dynamic query context built upon the source document to proceed.
+	 * 
+	 * @return The DynamicQueryContext object initialized.
+	 */
+	public DynamicQueryContext getDynamicQueryContext() {
 		return dynamicContext;
 	}
-	
-	
-	private SaxonProcessor()
-	{
+
+	private SaxonProcessor() {
 		initProcessor();
 	}
-	
-	private void initProcessor()
-	{
+
+	/**
+	 * Initializes the saxon processor.
+	 *
+	 */
+	private void initProcessor() {
 		// Creating configuration
 		config = new Configuration();
-		// Creating static context  : used for compile xquery
+		// Creating static context : used for compile xquery
 		staticContext = new StaticQueryContext(config);
 		// Creating dynamic context : used for process xquery
 		dynamicContext = new DynamicQueryContext(config);
 	}
-	
-	
-	
+
 }
--- CodeSquale/src/com/codesquale/metrics/saxon/SaxonQueryProvider.java
+++ CodeSquale/src/com/codesquale/metrics/saxon/SaxonQueryProvider.java
@@ -9,104 +9,112 @@ import org.apache.log4j.Logger;
 
 import com.codesquale.utils.Utilities;
 
+/**
+ * Provides the specific xquery request needed to calculates counters and
+ * metrics.
+ * 
+ * @author dwillier
+ * 
+ */
 public class SaxonQueryProvider {
-	
+
 	private static Logger logger = Logger.getLogger(SaxonProcessor.class);
-	
+
 	private final String SINGLE_FILE_COUNTING_QUERY_PATH = "xml\\XQuery\\saxon\\DirectoryFileCounters.xquery";
 
 	private XQueryExpression singleFileCountingQuery = null;
+
 	private XQueryExpression numberOfClasses = null;
+
 	private XQueryExpression numberOfPrivateClasses = null;
+
 	private XQueryExpression numberOfPublicClasses = null;
-	
+
+	/**
+	 *  Private instance of the class itselft needed by the singleton mechanism.
+	 */
 	private static SaxonQueryProvider _instance = null;
-	
-	public static SaxonQueryProvider getInstance()
-	{
-		if(_instance==null) _instance = new SaxonQueryProvider();
-		
+
+	/**
+	 * Methods that returns the singleton instance of the class.
+	 * @return A unique SaxonQueryProvider reference.
+	 */
+	public static SaxonQueryProvider getInstance() {
+		if (_instance == null)
+			_instance = new SaxonQueryProvider();
+
 		return _instance;
 	}
-	
-	private SaxonQueryProvider()
-	{
+
+	private SaxonQueryProvider() {
 		compileQuery();
 	}
-	
-	private void compileQuery()
-	{
-		StaticQueryContext context = SaxonProcessor.getInstance().getStaticQueryContext(); 
-		
-		try 
-		{
-			 singleFileCountingQuery = context.compileQuery(getSingleFileCountingQuery());
-			 numberOfClasses = context.compileQuery(getNumberOfClassesQuery());
-			 numberOfPrivateClasses = context.compileQuery(getNumberOfPrivateClassesQuery()); 
-			 numberOfPublicClasses = context.compileQuery(getNumberOfPublicClassesQuery()); 
-		} 
-		catch (XPathException ex) 
-		{
+
+	/**
+	 * Compile all the query contained in the SaxonQueryProvider class.
+	 * 
+	 */
+	private void compileQuery() {
+		StaticQueryContext context = SaxonProcessor.getInstance()
+				.getStaticQueryContext();
+
+		try {
+			singleFileCountingQuery = context
+					.compileQuery(getSingleFileCountingQuery());
+			numberOfClasses = context.compileQuery(getNumberOfClassesQuery());
+			numberOfPrivateClasses = context
+					.compileQuery(getNumberOfPrivateClassesQuery());
+			numberOfPublicClasses = context
+					.compileQuery(getNumberOfPublicClassesQuery());
+		} catch (XPathException ex) {
 			logger.fatal(ex.getMessage());
-		} 
+		}
 	}
-	
+
 	/*
-	 *  XQuery object
+	 * XQuery object
 	 */
-	
-	
-	public XQueryExpression getNumberOfClassesQueryObject()
-	{
-		return numberOfClasses ;
+
+	public XQueryExpression getNumberOfClassesQueryObject() {
+		return numberOfClasses;
 	}
-	
-	public XQueryExpression getNumberOfPrivateClassesQueryObject()
-	{
-		return numberOfPrivateClasses ;
+
+	public XQueryExpression getNumberOfPrivateClassesQueryObject() {
+		return numberOfPrivateClasses;
 	}
-	
-	public XQueryExpression getNumberOfPublicClassesQueryObject()
-	{
-		return numberOfPublicClasses ;
+
+	public XQueryExpression getNumberOfPublicClassesQueryObject() {
+		return numberOfPublicClasses;
 	}
-	
-	public XQueryExpression getSingleFileCountingQueryObject()
-	{
-		return singleFileCountingQuery ;
+
+	public XQueryExpression getSingleFileCountingQueryObject() {
+		return singleFileCountingQuery;
 	}
-	
-	
+
 	/*
-	 *  Query litterals
+	 * Query litterals
 	 */
-	
-	private String getSingleFileCountingQuery()
-	{
-		try 
-		{
+
+	private String getSingleFileCountingQuery() {
+		try {
 			return Utilities.readFileAsString(SINGLE_FILE_COUNTING_QUERY_PATH);
-		} catch (IOException e) 
-		{
+		} catch (IOException e) {
 			logger.fatal("The XQuery file seems to be unavailable.");
 		}
-		
+
 		return null;
 	}
-	
-	private  String getNumberOfClassesQuery()
-	{
+
+	private String getNumberOfClassesQuery() {
 		return "//directoryResult/counters/classes/all";
 	}
-	
-	private  String getNumberOfPrivateClassesQuery()
-	{
+
+	private String getNumberOfPrivateClassesQuery() {
 		return "//directoryResult/counters/classes/private";
 	}
-	
-	private  String getNumberOfPublicClassesQuery()
-	{
+
+	private String getNumberOfPublicClassesQuery() {
 		return "//directoryResult/counters/classes/public";
 	}
-	
+
 }
