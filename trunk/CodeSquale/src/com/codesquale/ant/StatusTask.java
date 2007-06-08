6117da3fdbc1981b49c55e3b8293f40067da8ad9
damien.cuvillier <damien.cuvillier@7896da65-2a2b-0410-bec6-9377b3eb570b>
2007-06-08 15:08:58 +0000

    [ADD] Replace echo task by personnalized StatusTask

---
+++ trunk/CodeSquale/src/com/codesquale/ant/StatusTask.java
@@ -0,0 +1,47 @@
+package com.codesquale.ant;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
+import org.apache.tools.ant.Task;
+/** <h1>Progress Ant Task </h1><BR>
+ * 
+ * Provides progress status indicators for CodeSquale global Process
+ * <br />
+ * 
+ * Uses Log4J Logger.
+ * @author Damien Cuvillier
+ *
+ */
+public class StatusTask extends Task {
+	
+	
+	/** Step indicator in the global Process <br />*/
+	private int step;
+	
+	private String message;
+	
+	private int levelLogger;
+	
+	/** AspectJ Log
+	 */
+	public void execute() {
+		System.out.println(message);
+	}
+	/** Step Variable setter. */
+	public void setStep(int step) {
+		this.step = step;
+	}
+	/** Message Accessor.
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public void setLevelLogger(int levelLogger){
+		
+	}
+}
+/** Defines all CodeSquale possible status.
+ * @author DCUVILLIER
+ *
+ */
