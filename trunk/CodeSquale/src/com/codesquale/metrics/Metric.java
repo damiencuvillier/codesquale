733bdd50895fce36af4482a0b0934a901c0e8179
damien.willier <damien.willier@7896da65-2a2b-0410-bec6-9377b3eb570b>
2007-06-15 15:35:59 +0000

    [REF] Checkstyle complying update.

--- trunk/CodeSquale/src/com/codesquale/metrics/Metric.java
+++ trunk/CodeSquale/src/com/codesquale/metrics/Metric.java
@@ -2,27 +2,54 @@ package com.codesquale.metrics;
 
 import net.sf.saxon.query.XQueryExpression;
 
-/*
- * Represent the main characteristics of a metric
+/**
+ * Represent and descrive the main characteristics of a metric.
+ * 
+ * @author dwillier
+ * 
  */
 public class Metric {
-	
+
+	/**
+	 * Represents the literal string query.
+	 */
 	private String metricXQueryExpression;
 
+	/**
+	 * metricDescription is the verbose description of the metric.
+	 */
 	private String metricDescription;
 
+	/**
+	 * metricLongName represents the long name of the metric.
+	 */
 	private String metricLongName;
 
+	/**
+	 * metricShortName is the short name of the metric, usually on four
+	 * characters.
+	 */
 	private String metricShortName;
 
+	/**
+	 * Defines if the metric is enabled or not.
+	 */
 	private boolean metricEnabled;
-	
+
+	/**
+	 * Defines the return value type of the metric. For moment two return value
+	 * types are considered : "single" or "list".
+	 */
 	private String metricType;
-	
+
+	/**
+	 * It is the compiled object from  
+	 */
 	private XQueryExpression compiledQuery;
-	
+
 	public Metric(String queryExpression, String metricDescription,
-			String metricLongName, String metricShortName, boolean enabled,String metricType, XQueryExpression compiledQuery) {
+			String metricLongName, String metricShortName, boolean enabled,
+			String metricType, XQueryExpression compiledQuery) {
 		this.metricXQueryExpression = queryExpression;
 		this.metricDescription = metricDescription;
 		this.metricLongName = metricLongName;
@@ -64,6 +91,4 @@ public class Metric {
 		return metricType;
 	}
 
-	
-	
 }
